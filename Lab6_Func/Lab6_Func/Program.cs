f5ca66283e3ede037bb7807b5e18f8c811579e15
IgorKulduaev <and.sobol39@mail.ru>
2025-12-22 13:32:32 +0200

    Add files via upload

---
+++ Lab6_Func/Lab6_Func/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//Базовый уровень. 1 вариант.
+using System.ComponentModel;
+Console.Write("Введите a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите b: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр a + b = {Func(a)+Func(b)}");
+    int Func(int i)
+{
+    if (i >= 10 && i <= 99)
+    {
+        int des = i / 10;
+
+        int ed = i % 10;
+        int sum = des+ed;
+        return sum;
+    }
+    else
+    {
+        return 0;
+    }
+}
